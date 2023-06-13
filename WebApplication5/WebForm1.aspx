3c135af20b59a41a4db355e70882ffafa25259c8
HILDA <HILDA@WIN-BNORA0NIVEB>
2023-06-13 10:59:37 +0300

    Add project files.

---
+++ WebApplication5/WebForm1.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication5.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server" >
+        <div>
+            <table align="center">
+                <tr>
+                    <td>ID:</td>
+                    <td>
+                        <asp:TextBox ID="txtID" runat="server"></asp:TextBox></td>
+                </tr>
+                 <tr>
+                    <td>Name:</td>
+                    <td>
+                        <asp:TextBox ID="txtName" runat="server"></asp:TextBox></td>
+                </tr>
+                 <tr>
+                    <td>Phone:</td>
+                    <td>
+                        <asp:TextBox ID="txtPhone" runat="server"></asp:TextBox></td>
+                </tr>
+                 <tr>
+                    <td>Adress:</td>
+                    <td>
+                        <asp:TextBox ID="txtAdress" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td align="center" colspan="2">
+                        <asp:Button ID="btnInsert" runat="server" Text="Insert" OnClick="btnInsert_Click" />
+                        <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+                        <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" style="height: 35px" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication5/WebForm1.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=WIN-BNORA0NIVEB\SQLEXPRESS;Initial Catalog=PRACTISE;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand(@"
+INSERT INTO[dbo].[Table1]
+           ([ID]
+           ,[name]
+           ,[phone]
+           ,[Adress])
+     VALUES
+           ('" +txtID.Text+ "','" +txtName.Text+ "','" +txtPhone.Text+ "','" +txtAdress.Text+ "')", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            Response.Write("Data Inserted successfully");
+            con.Close();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=WIN-BNORA0NIVEB\SQLEXPRESS;Initial Catalog=PRACTISE;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand(@"DELETE FROM [dbo].[Table1]
+      WHERE [ID]='" +txtID.Text+ "'",con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            Response.Write("Data Deleted successfully");
+            con.Close();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=WIN-BNORA0NIVEB\SQLEXPRESS;Initial Catalog=PRACTISE;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand(@"UPDATE [dbo].[Table1]
+   SET [ID] ='" + txtID.Text + "',[name] ='" + txtName.Text + "',[phone] = '" + txtPhone.Text + "',[Adress] = '" + txtAdress.Text + "' WHERE [ID]='" + txtID.Text + "'", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            Response.Write("Data Deleted successfully");
+            con.Close();
+        }
+    }
+}
